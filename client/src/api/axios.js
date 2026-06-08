1216dfa58b594f4de3da99d6c4434f8a364aa0d3
razurazaan <cmdrazaan@gmail.com>
2026-06-08 15:50:08 +0530

    first commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+});
 
 api.interceptors.response.use(
   res => res,
   err => {
     if (err.response?.status === 401) {
       localStorage.removeItem("token");
-      window.location.href = "/login";
+      window.location.href = `${import.meta.env.BASE_URL}login`;
     }
     return Promise.reject(err);
   }
