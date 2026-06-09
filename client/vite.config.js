e4bbc60b9db9dfa87fa60b3cd7663239b92e8d41
razurazaan <cmdrazaan@gmail.com>
2026-06-09 12:50:39 +0530

    fix: add vercel.json, update vite config, ignore dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/quiz-app/",
+  
+//   server: {
+//     port: 5173,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/quiz-app/",
-  
   server: {
     port: 5173,
     proxy: {
@@ -13,5 +30,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
