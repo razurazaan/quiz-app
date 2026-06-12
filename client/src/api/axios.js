ea617b15a1cf5ba5e05fb3fd5ccd03818745baf6
Razaan <113543046+razurazaan@users.noreply.github.com>
2026-06-12 14:34:18 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+    baseURL: "http://localhost:5000/api",
+
 });
 
 api.interceptors.response.use(
